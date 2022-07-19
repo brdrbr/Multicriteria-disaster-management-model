abe1b654c00687628f7ee3768c92a2ad055267e8
Berke Derin Berktay <98432576+brdrbr@users.noreply.github.com>
2022-07-19 13:27:08 +0300

    Update objective1.py

--- objective1.py
+++ objective1.py
@@ -1,155 +1,219 @@
-from variables import *
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 14 06:08:17 2022
+@author: Computer1
+"""
+from declaration_edge import *
 from pyomo.opt import SolverFactory
-
 
 Model = ConcreteModel()
 
-# We need time periods too. I kept it as 1 for simplicity.
-# I also kept commodity number as 1 for simplicity.
+# OBJECTIVE 2
+"""alpha = 1
+objective = 0
+for t in nT:
+    for k in nK:
+        for d in nD:
+            objective += (Model.D[d, k, t] - Model.H[d, k, t]) * math.exp((-alpha)*t)
+# Sense = 1 is maximizing
+Model.obj = Objective(expr=objective, sense=-1)
+Model.c1 = ConstraintList()"""
 
-# Amount of commodity k sent on arc (i,j) in period t
-Model.X = Var(nS, nD, within=NonNegativeReals)
-# I assumed nodes cannot feed themselves, we can remove these...
-Model.X[1, 1] = 0
-Model.X[2, 2] = 0
-Model.X[3, 3] = 0
-Model.X[4, 4] = 0
-Model.X[5, 5] = 0
-Model.X[6, 6] = 0
+# Amount of commodity k sent on arc e in period t
+Model.X = Var(nE, nK, nT, within=NonNegativeReals)
+for t in nT:
+    for k in nK:
+        Model.X[11, k, t] = 0
+        Model.X[22, k, t] = 0
+        Model.X[33, k, t] = 0
+        Model.X[44, k, t] = 0
+        Model.X[55, k, t] = 0
+        Model.X[66, k, t] = 0
 
 # Demand of commodity k at node j in period t
-Model.D = Var(nD, bounds=(0, np.inf), within=NonNegativeReals)
-Model.D[1] = djkt[0][0][1]
-Model.D[2] = djkt[0][0][2]
-Model.D[3] = djkt[0][0][3]
-Model.D[4] = djkt[0][0][4]
-Model.D[5] = djkt[0][0][5]
-Model.D[6] = djkt[0][0][6]
-
+Model.D = Var(nD, nK, nT, bounds=(0, np.inf), within=NonNegativeReals)
 
 # Unsatisfied demand of commodity k at node j in period t
-Model.H = Var(nD, bounds=(0, 0), within=NonNegativeReals) # 0 for t-1 = 0 case.
-Model.H[1] = 0
-Model.H[2] = 0
-Model.H[3] = 0
-Model.H[4] = 0
-Model.H[5] = 0
-Model.H[6] = 0
+Model.H = Var(nD, nK, nT, bounds=(0, np.inf), within=NonNegativeReals)  # 0 for t-1 = 0 case.
+for k in nK:
+    Model.H[1, k, 0] = 0
+    Model.H[2, k, 0] = 0
+    Model.H[3, k, 0] = 0
+    Model.H[4, k, 0] = 0
+    Model.H[5, k, 0] = 0
+    Model.H[6, k, 0] = 0
 
-# Units of resources allocated to blocked arc (i,j) in period t
-Model.W = Var(nS, nD, bounds=(0, np.inf), within=NonNegativeReals)
-Model.W[1, 1] = 0
-Model.W[2, 2] = 0
-Model.W[3, 3] = 0
-Model.W[4, 4] = 0
-Model.W[5, 5] = 0
-Model.W[6, 6] = 0
+# Units of resources allocated to blocked arc e in period t
+Model.W = Var(nE, nT, bounds=(0, np.inf), within=NonNegativeReals)
+for t in nT:
+    Model.W[11, t] = 0
+    Model.W[22, t] = 0
+    Model.W[33, t] = 0
+    Model.W[44, t] = 0
+    Model.W[55, t] = 0
+    Model.W[66, t] = 0
 
-# 1 if blocked arc (i,j) is open in period t, 0, otherwise
-Model.Y = Var(nS, nD, within=Binary)
-Model.Y[1, 1] = 0
-Model.Y[2, 2] = 0
-Model.Y[3, 3] = 0
-Model.Y[4, 4] = 0
-Model.Y[5, 5] = 0
-Model.Y[6, 6] = 0
+# 1 if blocked arc e is open in period t, 0, otherwise
+Model.Y = Var(nE, nT, within=Binary)
+for t in nT:
+    Model.Y[11, t] = 0
+    Model.Y[22, t] = 0
+    Model.Y[33, t] = 0
+    Model.Y[44, t] = 0
+    Model.Y[55, t] = 0
+    Model.Y[66, t] = 0
 
 # OBJECTIVE
 objective = 0
-for i in Cijkt[0][0]:
-    for e in nS:
-        for k in nD:
-            if i[0] == e and i[1] == k:
-                objective += i[2]*Model.X[e, k]
+for t in nT:
+    for k in nK:
+        for i in Cijkt[t][k]:
+            for e in nE:
+                if i[0] == e:
+                    objective += i[1]*Model.X[e, k, t]
 # Sense = 1 is minimizing
 Model.obj = Objective(expr=objective, sense=1)
 Model.c1 = ConstraintList()
 
-
 # CONSTRAINT 1
-for e in Ik[0]:  # for all supply nodes
-    if e in [1, 2]:
-        Model.c1.add(sum(Model.X[e, k] - Model.X[k, e] for k in nD) == Sikt[0][0][e])
+into = 0
+out = 0
+for t in nT:
+    for k in nK:
+        for e in nS:  # for all supply nodes
+            if Sikt[t][k][e] > 0:
+                for i in edge_dict.keys():
+                    if int(str(i)[:-1]) == e:
+                        into += Model.X[i, k, t]
+                    if int(str(i)[1:2]) == e:
+                        out += Model.X[i, k, t]
+        
+                Model.c1.add(into - out == Sikt[t][k][e])
+                into = 0
+                out = 0
 
 
 # CONSTRAINT 2
-for e in Jk[0]:  # for all demand nodes
-    if e in [3, 4]:
-        Model.c1.add((sum(Model.X[k, e] - Model.X[e, k] for k in nS) + Model.H[e] == Model.D[e]))
+into = 0
+out = 0
+for t in nT:
+    for k in nK:
+        for e in nD:  # for all demand nodes
+            if djkt[t][k][e] > 0:
+                for i in edge_dict.keys():
+                    if int(str(i)[:-1]) == e:
+                        out += Model.X[i, k, t]
+                    if int(str(i)[1:2]) == e:
+                        into += Model.X[i, k, t]
+        
+                Model.c1.add(into - out + Model.H[e, k, t] == Model.D[e, k, t])
+                into = 0
+                out = 0
 
 
 # CONSTRAINT 3
-for e in Tk[0]:  # for all transition nodes ( neither supply nor demand )
-    if e in [5, 6]:
-        Model.c1.add(sum(Model.X[k, e] for k in nS) - sum(Model.X[e, k] for k in nS) == 0)
+for t in nT:
+    for k in nK:
+        for e in nN:  # for all transition nodes ( neither supply nor demand )
+            if Sikt[t][k][e] == 0 and djkt[t][k][e] == 0:
+                for i in edge_dict.keys():
+                    if int(str(i)[:-1]) == e:
+                        out += Model.X[i, k, t]
+                    if int(str(i)[1:2]) == e:
+                        into += Model.X[i, k, t]
+        
+                Model.c1.add(into - out == 0)
+                into = 0
+                out = 0
 
 
-# CONSTRAINT 4 - I removed this for now, since we only consider 1 time period.
-for d in nD:
-    # assume for now t-1 = 0
-    Model.c1.add(Model.D[d] == Model.H[d] + djkt[0][0][d])
+# CONSTRAINT 4
+for t in nT:
+    for k in nK:
+        for d in nD:
+            if t == 0:
+                Model.c1.add(Model.H[d, k, t] == 0)
+                Model.c1.add(Model.D[d, k, t] == djkt[t][k][d])
+            else:
+                Model.c1.add(Model.D[d, k, t] == Model.H[d, k, t-1] + djkt[t][k][d])
 
 
 # REMOVING BLOCKED ARCS FOR CONSTRAINTS 5 AND 6
 capacities = []
-for i in uijt[0][0]:
-    for e in nS:
-        for k in nD:
-            if i[0] == e and i[1] == k:
-                capacities.append(i[2])
+for t in nT:
+    for i in uijt[t]:
+        for s in nS:
+            for d in nD:
+                if int(str(i[0])[:-1]) == s and int(str(i[0])[1:2]) == d:
+                    capacities.append(i[1])
+
+print(capacities)
 
 # CONSTRAINT 5
-for index, i in enumerate(uijt[0][0]):
-    if i[2] != 0:  # its not blocked
-        Model.c1.add(Model.X[i[0], i[1]] <= capacities[index])
-
+for t in nT:
+    for k in nK:
+        for index, i in enumerate(uijt[t]):
+            if i[1] != 0:  # its not blocked
+                Model.c1.add(Model.X[i[0], k, t] <= capacities[index])
 
 # CONSTRAINT 6
-for index, i in enumerate(uijt[0][0]):
-    if i[2] == 0:  # its blocked
-        Model.c1.add(Model.X[i[0], i[1]] <= capacities[index] * Model.Y[i[0], i[1]])
+for t in nT:
+    for k in nK:
+        for index, i in enumerate(uijt[t]):
+            if i[1] == 0:  # its blocked
+                Model.c1.add(Model.X[i[0], k, t] <= capacities[i[1]] * Model.Y[i[0], t])
 
 
 # CONSTRAINT 7
 origin_list = []
 destination_list = []
-for index, i in enumerate(uijt[0][0]):
-    if i[2] == 0:  # its blocked
-        origin_list.append(i[0])
-        destination_list.append(i[1])
-Model.c1.add(sum(Model.W[o, d] for (o, d) in zip(origin_list, destination_list)) <= sum([bt[0][a][2] for a in range(len(bt[0]))]))
+time_list = []
+for t in nT:
+    for index, i in enumerate(uijt[t]):
+        if i[1] == 0:  # its blocked
+            origin_list.append(int(str(i[0])[:-1]))
+            destination_list.append(int(str(i[0])[1:2]))
+            time_list.append(t)
+
+Model.c1.add(sum(Model.W[int(str(o)+str(d)), t] for (o, d, t) in zip(origin_list, destination_list, time_list)) <= sum([bt[0][a][1] for a in range(len(bt[0]))]))
 
 
 # CONSTRAINT 8
 origin_list = []
 destination_list = []
+time_list = []
 blocked_indexes = []
 counter = 0
-for index, i in enumerate(uijt[0][0]):
-    if i[2] == 0:  # its blocked
-        origin_list.append(i[0])
-        destination_list.append(i[1])
-        blocked_indexes.append(counter)
-        Model.c1.add(Model.W[i[0], i[1]] >= aij[0][0][counter][2] * Model.Y[i[0], i[1]])
-        counter += 1
-
+for t in nT:
+    for index, i in enumerate(uijt[t]):
+        if i[1] == 0:  # its blocked
+            origin_list.append(int(str(i[0])[:-1]))
+            destination_list.append(int(str(i[0])[1:2]))
+            time_list.append(t)
+            blocked_indexes.append(counter)
+            Model.c1.add(Model.W[i[0], t] >= aij[counter][1] * Model.Y[i[0], t])
+            counter += 1
 
 # CONSTRAINT 9
-for index, i in enumerate(uijt[0][0]):
-    if i[2] == 0:
-        Model.c1.add(Model.X[i[0], i[1]] >= 0)
-        Model.c1.add(Model.D[i[1]] >= 0)
-        Model.c1.add(Model.H[i[1]] >= 0)
+for t in nT:
+    for k in nK:
+        for index, i in enumerate(uijt[t]):
+            if i[1] == 0:
+                Model.c1.add(Model.X[int(str(i[0])),k ,t] >= 0)
+                Model.c1.add(Model.D[int(str(i[0])[1:2]),k ,t] >= 0)
+                Model.c1.add(Model.H[int(str(i[0])[1:2]),k ,t] >= 0)
 
 counter = 0
-for index, i in enumerate(uijt[0][0]):
-    if i[2] != 0:
-        if counter == 2:
-            break
-        Model.c1.add(Model.X[i[0], i[1]] >= 0)
-        Model.c1.add(Model.D[i[1]] >= 0)
-        Model.c1.add(Model.H[i[1]] >= 0)
-        counter += 1
+for t in nT:
+    for k in nK:
+        for index, i in enumerate(uijt[t]):
+            if i[1] != 0:
+                if counter == 2:
+                    break
+                Model.c1.add(Model.X[int(str(i[0])),k ,t] >= 0)
+                Model.c1.add(Model.D[int(str(i[0])[1:2]),k ,t] >= 0)
+                Model.c1.add(Model.H[int(str(i[0])[1:2]),k ,t] >= 0)
+                counter += 1
 
 Model.obj.pprint()
 Model.c1.pprint()
